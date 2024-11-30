d92db4875381ae861d18a2b67810d0967055d77b
C4r50nC <xichen059@gmail.com>
2024-12-01 10:21:05 +1100

    feat: add AccountsController

---
+++ ContactsManager.UI/Controllers/AccountsController.cs
@@ -0,0 +1,21 @@
+﻿using ContactsManager.Core.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsManager.UI.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AccountsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterDto registerDto)
+        {
+            return RedirectToAction(nameof(PersonsController.Index), nameof(PersonsController));
+        }
+    }
+}
