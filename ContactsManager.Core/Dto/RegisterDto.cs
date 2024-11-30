c7785b2d8bca5b45f087f0f515eb8226320f4764
C4r50nC <xichen059@gmail.com>
2024-12-01 10:19:06 +1100

    feat: add DTO for account registration

---
+++ ContactsManager.Core/Dto/RegisterDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactsManager.Core.Dto
+{
+    public class RegisterDto
+    {
+        [Required]
+        public required string PersonName { get; set; }
+        [Required]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        public required string Email { get; set; }
+        [Required]
+        [RegularExpression("^[0-9]*$")]
+        [DataType(DataType.PhoneNumber)]
+        public required string Phone { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public required string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public required string ConfirmPassword { get; set; }
+    }
+}
